bcd21c4b1e84e5cf72f0e4ff3ccb1f26f0f64618
Mazkid21 <zmaraziti@yahoo.com>
2017-09-13 23:22:19 -0600

    added pages with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,33 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from '../environments/environment';
 import { AuthService } from './shared/auth.service'
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginComponent } from './login/login.component';
+
+const appRoutes: Routes = [
+  
+  { path: '', component: LandingPageComponent},
+  { path: 'login', component: LoginComponent}
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase, 'BackBy'),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+     RouterModule.forRoot(
+    appRoutes
+    )
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
